3525cec1b68243ea9e0204de6a4df99ff86ddd2b
VPsomak <vpsomak@mail.ntua.gr>
2026-05-11 13:38:53 +0300

    added license

--- src/monitor.py
+++ src/monitor.py
@@ -1,4 +1,20 @@
-# monitor.py
+# Copyright (C) 2026 Efficient Computing Lab - NTUA <vpsomak@mail.ntua.gr>
+#
+# This file is part of InferLens.
+#
+# InferLens is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# InferLens is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with InferLens. If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 
 import csv
